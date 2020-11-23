3b1d639a7be38281ac20ddd4181d7fe6b4ade30d
github raamenzurururu <reku9696@gmail.com>
2020-11-23 09:42:48 +0900

    [add]onsaveを起動

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
-'use strict';
+"use strict";
 
+{
+  const year = 2020;
+  const month = 12; //11月
+
+  function getCalendarBody() {
+    const dates = [];
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push(i);
+    }
+
+    console.log(dates);
+  }
+
+  getCalendarBody();
+}
